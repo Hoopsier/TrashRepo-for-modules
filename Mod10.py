00401372a1d42d644730b48608ee6ecf7d395bf8
Renan <renanhoruz@gmail.com>
2025-10-22 11:10:01 +0300

    mod10 done

--- Mod10.py
+++ Mod10.py
@@ -1,42 +1,46 @@
-class Elain():
-    def pet():
+class Kone():
+    def tankaa():
         pass
-
-class Koira(Elain):
-    def __init__(self,vuosi:int,nimi:str,haukku:str = "Vuh Vuh!"):
-        self.nimi :str=nimi
+### NOTE: Why the hell does type annotation not do shit? Like screw you python creator! 
+class Boeing(Kone):
+    bensaMax : int = 2000
+    def __init__(self,vuosi:int,bensa:int,nimi:str):
+        self.nimi = nimi
         self.vuosi :int= vuosi
-        self.haukku :str = haukku
-    def pet(self):
-        print(self.haukku)
+        self.bensa :int = bensa
+    def tankaa(self):
+        self.bensa = self.bensaMax 
 
-class Kissa(Elain):
-    def __init__(self, vuosi:int,nimi:str, purina:str = "pur"):
-        self.nimi :str=nimi
+class TurkishAirline(Kone):
+    bensaMax : int = 4000
+    def __init__(self, vuosi:int, bensa:int,nimi:str):
+        self.nimi = nimi
         self.vuosi :int= vuosi
-        self.purina :str= purina
-    def pet(self):
-        print(self.purina)
+        self.bensa :int = bensa
+    def tankaa(self):
+        self.bensa = self.bensaMax 
         
 
-class Hoitola():
-    def __init__(self):
-        self.elaimet = []
-    def addElain(self, elain :Elain):
-        self.elaimet.append(elain)
-    def getElaimet(self):
-        for elain in self.elaimet:
-            print(elain.nimi)
-    def petElaimet(self):
-        for elain in self.elaimet:
-            elain.pet()
+class Kentta():
+    def __init__(self,nimi):
+        self.nimi = nimi
+        self.koneet = []
+    def addKone(self, kone :Kone):
+        self.koneet.append(kone)
+    def haeKoneet(self):
+        for kone in self.koneet:
+            print(f"{kone.nimi}: {kone.bensa} of gas")
+    def tankkaaKaikki(self):
+        for kone in self.koneet:
+            kone.tankaa()
 
-koira1 = Koira(1984,"Murre" )
-koira2 = Koira(1997,"Musti" )
-kissa1 = Kissa(1987,"Miuku")
-htola = Hoitola()
-htola.addElain(koira1)
-htola.addElain(koira2)
-htola.addElain(kissa1)
-htola.getElaimet()
-htola.petElaimet()
+kone1 = Boeing(1984,100,"Trash Heap")
+kone2 = Boeing(1997,400, "Trash Heap 2.0")
+kone3 = TurkishAirline(1987,2000, "Normal Plane")
+kentta= Kentta("Smallest Airport")
+kentta.addKone(kone1)
+kentta.addKone(kone2)
+kentta.addKone(kone3)
+kentta.haeKoneet()
+kentta.tankkaaKaikki()
+kentta.haeKoneet()
